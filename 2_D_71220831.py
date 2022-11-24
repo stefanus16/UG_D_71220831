5c7c6526f65a0752bf8d7a97b4a4a6e22085bb20
stefanus16 <117900966+stefanus16@users.noreply.github.com>
2022-11-24 16:14:30 +0700

    Add files via upload

---
+++ 2_D_71220831.py
@@ -0,0 +1,9 @@
+nama = input("masukan nama pemain pertama: ")
+nama = input("masukan nama pemain kedua: ")
+nama = input("masukan nama pemain ketiga: ")
+jumlah = input("masukan jumlah kartu pemain pertama: ")
+jumlah = input("masukan jumlah kartu pemain kedua: ")
+jumlah = input("masukan jumlah kartu pemain ketiga: ")
+
+if(jumlah >=21):
+    print("jumlah kartu melebihi batas")
